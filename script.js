538627d3abe7e0d05a8d05ad1e585c53761f9f71
shimamuzavvir <shima1997ibrahim@gmail.com>
2024-01-29 16:13:23 +0400

    end

--- script.js
+++ script.js
@@ -1,53 +1,48 @@
 function result(){
     var Firstname = document.getElementById("first-name").value;
     var t1 =document.getElementById("t1");
-    var fname =t1.append(Firstname);
-    document.body.append(fname);
+     t1 =t1.append(Firstname);
+    document.body.append(t1);
 
     var Lastname = document.getElementById("last-name").value;
     var t2 =document.getElementById("t2");
-    var lname =t2.append(Lastname);
-    document.body.append(lname);
+     t2 =t2.append(Lastname);
+    document.body.append(t2);
 
     var Address = document.getElementById("address").value;
     var t3 =document.getElementById("t3");
-    var add =t3.append(Address);
-    document.body.append(add);
+    t3=t3.append(Address);
+    document.body.append(t3);
 
     var Pincode = document.getElementById("pincode").value;
     var t4 =document.getElementById("t4");
-    var pin =t4.append(Pincode);
-    document.body.append(pin);
+    t4 =t4.append(Pincode);
+    document.body.append(t4);
 
     var Gender = document.getElementById("gender").value;
     var t5 =document.getElementById("t5");
-    var gen =t5.append(Gender);
-    document.body.append(gen);
+    t5 =t5.append(Gender);
+    document.body.append(t5);
     
-    var checkboxes = document.querySelectorAll('input[type="checkbox"]');
-    var selectedCheckboxes = Array.from(checkboxes).filter(checkbox => checkbox.checked);
-    var t6 = document.getElementById('t6');
-    t6.innerHTML = '<td id="t6"></td>';
-    selectedCheckboxes.forEach(function(checkbox) {
-        var checkboxLabel = document.createElement('p');
-        checkboxLabel.textContent = checkbox.value;
-        t6.appendChild(checkboxLabel);
-    });
+  var food = document.getElementsByClassName("food");
+  var checkedbox=[];
+  for(let i=0; i<food.length; i++){
+    if(food[i].checked){
+    checkedbox.push(food[i].value)
+  }}
+  var t6 = document.getElementById("t6");
+  checkedbox = checkedbox.join(",");
+  t6 = t6.append(checkedbox);
+  document.body.append(t6)
 
     
     var State = document.getElementById("state").value;
     var t7 =document.getElementById("t7");
-    var sta =t7.append(State);
-    document.body.append(sta);
+    t7 =t7.append(State);
+    document.body.append(t7);
 
     var Country = document.getElementById("country").value;
     var t8 =document.getElementById("t8");
-    var count =t8.append(Country);
-    document.body.append(count);
-
-    
-
-
-   
-    
+    t8 =t8.append(Country);
+    document.body.append(t8);
 }
